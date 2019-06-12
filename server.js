380a94d9b860f4e1d5e5587c03e011ebab8903ba
Gustavo Bravo <guxtab@gmail.com>
2019-06-12 16:19:07 -0300

    Add option to remove array (https://github.com/typicode/json-server/issues/259)

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3004;
+
+router.render = function (req, res) {
+	console.log(res.locals.data.length);
+  if (res.locals.data.length == 1) {
+    res.locals.data = res.locals.data[0] // "extracts" first item
+  }
+  res.jsonp(res.locals.data)
+}
+
 
 server.use(middlewares);
 server.use(router);
